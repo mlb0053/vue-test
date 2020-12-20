c2aafa7d4ceb5c50f4d59d91a0116df2ccb90f17
e <2607887171@qq.com>
2020-12-20 21:59:31 +0800

    基础

---
+++ src/views/Login/index.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>123</div>
+</template>
+
+<script>
+export default {
+    data(){
+        return
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+#login {
+  height: 100vh;
+  background-color: #344a5f;
+}
+.login-wrap {
+  width: 330px;
+  margin: auto;
+}
+.menu-tab {
+  text-align: center;
+  li {
+    display: inline-block;
+    width: 88px;
+    line-height: 36px;
+    font-size: 14px;
+    color: #fff;
+    border-radius: 2px;
+    cursor: pointer;
+  }
+  .current {
+    background-color: rgba(0, 0, 0, .1);
+  }
+}
+
+</style>
